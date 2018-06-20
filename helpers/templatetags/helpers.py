d0da13bd6aec2aad887ece0388725f22976c8925
Cyril Margorin <cyril@imperx.com>
2018-06-20 18:06:46 +0300

    if django < 2.1, import json_script code Django v2.1 wipe out debug code

--- helpers/templatetags/helpers.py
+++ helpers/templatetags/helpers.py
@@ -1,5 +1,6 @@
-from django import template
+from django import template, get_version
 from django.conf import settings
+from pkg_resources import parse_version
 
 register = template.Library()
 
@@ -14,3 +15,36 @@ def key(d, key_name):
         value = settings.TEMPLATE_STRING_IF_INVALID
 
     return value
+
+
+if parse_version(get_version()) < parse_version("2.1"):
+    @register.filter(is_safe=True)
+    def json_script(value, element_id):
+        _json_script_escapes = {
+            ord('>'): '\\u003E',
+            ord('<'): '\\u003C',
+            ord('&'): '\\u0026',
+        }
+
+        def _json_script(value, element_id):
+            """
+            Escape all the HTML/XML special characters with their unicode escapes, so
+            value is safe to be output anywhere except for inside a tag attribute. Wrap
+            the escaped JSON in a script tag.
+            """
+            from django.core.serializers.json import DjangoJSONEncoder
+            import json
+            from django.utils.html import format_html
+            from django.utils.safestring import mark_safe
+
+            json_str = json.dumps(value, cls=DjangoJSONEncoder).translate(_json_script_escapes)
+            return format_html(
+                '<script id="{}" type="application/json">{}</script>',
+                element_id, mark_safe(json_str)
+            )
+
+        """
+        Output value JSON-encoded, wrapped in a <script type="application/json">
+        tag.
+        """
+        return _json_script(value, element_id)
